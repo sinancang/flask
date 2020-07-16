d755bb8d4eada55557a3c20b4ecad8d33d926dff
sinancang <sinancangulan@gmail.com>
2020-07-16 17:16:18 +0300

    user authentication stuff

--- app/main/views.py
+++ app/main/views.py
@@ -1,28 +1,9 @@
-from flask import current_app, flash, make_response, render_template, session, redirect, url_for
+from flask import render_template, make_response 
 from . import main
-from .forms import NameForm
-from ..meta import db
-from ..models import User
-from ..email import send_email
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you\'ve changed your name!')
-        session['name'] = form.name.data
-        user = User(username=form.name.data,
-                email=form.mail.data,
-                password=form.pass1.data,
-                gender=form.gender.data,
-                role_id = 1)
-        send_email(form.mail.data, 'New User', 'mail/new_user', user=user)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('.index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html')
 
 @main.route('/redirect_me')
 def redirect_me():
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from .meta import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from flask import current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -17,11 +19,26 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64))
-    hash_password = db.Column(db.String(64))
     gender = db.Column(db.String(64))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm':self.id}).decode('utf-8')
+    
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
