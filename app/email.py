c41cf46cb64d23d79beed5db24236e1b36134886
sinancang <sinancangulan@gmail.com>
2020-07-24 14:51:32 +0300

    user permissions

--- app/email.py
+++ app/email.py
@@ -1,17 +1,19 @@
 from flask import Flask, current_app, render_template
-from flask_mail import Message, Mail
+from flask_mail import Message
+from .meta import mail
 from threading import Thread
+import time
 
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
-        msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=current_app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+    app = current_app._get_current_object()
+    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=current_app.config['FLASKY_MAIL_SENDER'],recipients=[to])
 
-        msg.body = render_template(template + '.txt', **kwargs)
-        msg.html = render_template(template + '.txt', **kwargs)
-        thr = Thread(target=send_async_email, args=[current_app, msg])
-        thr.start()
-        return thr
-
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.txt', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
