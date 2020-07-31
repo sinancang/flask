420f364d515715b066022fcb9a1be63385c99435
sinancang <sinancangulan@gmail.com>
2020-07-31 17:54:13 +0300

    role assignment bug fix

--- app/models.py
+++ app/models.py
@@ -98,8 +98,10 @@ class User(UserMixin, db.Model):
         super(User, self).__init__(**kwargs)
         if self.role is None:
             if self.email == current_app.config['FLASKY_ADMIN']:
-                self.role = Role.query.filter_by(name='Administrator').first()
-            if self.role is None:
+                print("set role to admin")
+                self.role = Role.query.filter_by(name='Admin').first()
+            else:
+                print("set role to default")
                 self.role = Role.query.filter_by(default=True).first()
 
     def can(self, perm):
