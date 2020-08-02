b0147fda94aa71c019d72dbc7132f0660c5904aa
sinancang <sinancangulan@gmail.com>
2020-08-02 21:16:53 +0300

    add user profile editing

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template, make_response
+from flask import render_template, make_response, redirect, url_for, flash
 from . import main
 from ..models import User
+from flask_login import login_required, current_user
+from .forms import EditProfileForm
+from ..meta import db
 
 @main.route('/')
 def index():
@@ -16,3 +19,22 @@ def redirect_me():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
