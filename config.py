da5c8f5c35d19d69ad5053b2f45c1591a559b890
sinancang <sinancangulan@gmail.com>
2021-09-03 13:08:04 -0400

    test commit

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
-
+ 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
